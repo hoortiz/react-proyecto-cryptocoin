c80549808c824be0ac9ea1e969fbeeb1b2ac6799
Henrry Ortiz <hoortiz@gmail.com>
2023-09-28 20:55:44 -0500

    Arreglo de la grafica

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     const [search, setSearch] = useState('');
 
     const getData = async () =>{
-//const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&sparkline=true&page=1")
+    const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&sparkline=true&page=1")
     /*const res = {data:
 [
     {
